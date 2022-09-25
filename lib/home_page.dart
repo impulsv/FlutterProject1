d395d8c8547e3dc07c74f774ef7cc246b78fbd96
impulsv <104969397+impulsv@users.noreply.github.com>
2022-09-26 00:46:58 +0530

    day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  final int days = 1;
+  final String name = "V";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("Catalogue App"),
+      ),
+      body: Center(
+        child: Container(child: Text("Welcome $name, you are #$days!"),
+        ),
+      ),
+    drawer: Drawer(),
+    );
+  }
+}
