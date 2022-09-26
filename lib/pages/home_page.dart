9a01489a7a83e74b997efe4eea938d1e5c86699f
impulsv <104969397+impulsv@users.noreply.github.com>
2022-09-26 22:24:29 +0530

    Day 3 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-class homePage extends StatelessWidget {
+class HomePage extends StatelessWidget {
   final int days = 1;
   final String name = "V";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title: Text("Catalogue App"),
+        title: Text("Catalogue App"),
       ),
       body: Center(
-        child: Container(child: Text("Welcome $name, you are #$days!"),
+        child: Container(
+          child: Text("Welcome $name, you are #$days !"),
         ),
       ),
-    drawer: Drawer(),
+      drawer: Drawer(),
     );
   }
 }
